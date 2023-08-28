f30403468f1994c933733bcac002778c2eb46896
GomesOsvaldo <94185529+GomesOsvaldo@users.noreply.github.com>
2023-08-29 00:39:32 +0100

    Rewind V2
    Rewind second update - v2

--- js/site.js
+++ js/site.js
@@ -1,4 +1,34 @@
-// Call Hello World
-function helloWorld() {
-  alert("Hello world");
+//Get the string from the page.
+//Controller fuction
+function getValue() {
+  document.getElementById("alert").classList.add("invisible");
+
+  let userString = document.getElementById("userString").value;
+
+  let revString = reverseString(userString);
+
+  displayString(revString);
+}
+
+//Logic function
+// Reverse the string
+function reverseString(userString) {
+  let revString = [];
+
+  //Reverse a string using a for loop
+  for (let index = userString.length - 1; index >= 0; index--) {
+    revString += userString[index];
+  }
+  return revString;
+}
+
+// Display the reversed string to the user
+function displayString(revString) {
+  //Write to the page
+  document.getElementById(
+    "msg"
+  ).innerHTML = `Your string reversed is: ${revString}`;
+
+  //Show the alert box
+  document.getElementById("alert").classList.remove("invisible");
 }
